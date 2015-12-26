6ba0a681f750361f992241a38b3c0eccdf9bae4e
Ethan Totten <thethan@thethan.com>
2015-12-26 13:51:25 -0800

    redirect path

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
     | a simple trait to add these behaviors. Why don't you explore it?
     |
     */
-    public $redirectPath = '/auth/classrooms';
+    public $redirectPath = '/admin/classrooms';
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 

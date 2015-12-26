cdf973d0029a0c3110c5a55027f3701fa5b21220
Ethan Totten <thethan@thethan.com>
2015-12-26 01:04:34 -0800

    fix the form

--- resources/views/admin/tables/_form.blade.php
+++ resources/views/admin/tables/_form.blade.php
@@ -1,15 +1,10 @@
     <div class="form-group">
-        {!! Form::label('color', 'Color', array('class' => 'col-sm-2 control-label')) !!}
+        {!! Form::label('color', 'Table name (color)', array('class' => 'col-sm-2 control-label')) !!}
         <div class="col-sm-10">
-            {!! Form::text('color',$color,['class' => 'control-input form-control', 'placeholder' => 'color', 'id' => 'color'], $color) !!}
+            {!! Form::text('color',$color,['class' => 'control-input form-control', 'placeholder' => 'name', 'id' => 'color'], $color) !!}
         </div>
     </div>
-    <div class="form-group">
-        <label for="inputPassword3" class="col-sm-2 control-label">Grade</label>
-        <div class="col-sm-10">
-            {!! Form::select('table_id',$tables, $table_id, array('class' => 'form-control')) !!}
-        </div>
-    </div>
+
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
             <button type="submit" class="btn btn-default">Save</button>
